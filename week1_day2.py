1df4606f5de4ee5b84c36ce11623ec3aa25c3ceb
Kaoru2023 <purisika2021@gmail.com>
2025-07-10 12:39:47 +0900

    first_python_file_commit

---
+++ week1_day2.py
@@ -0,0 +1,23 @@
+# week1_day2.py
+# author: kaoru
+# date: 2025-07-10
+
+name = "Kaoru"
+age = 54
+height = 160.0
+is_programmer = True
+
+print(f"Hello, my name is {name}.")
+print(f"I am {age} years old.")
+print(f"My height is  {height} cm.")
+print(f"Am I a programmer? {is_programmer}")
+
+# Basic arithmetic
+num1 = 10
+num2 = 3
+print(f"Sum: {num1 + num2}")
+print(f"Difference: {num1 - num2}")
+print(f"Product: {num1 * num2}")
+print(f"Division: {num1 / num2}")
+print(F"Floor Division: {num1 // num2}")
+print(f"Modulo: {num1 % num2}")
